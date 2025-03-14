684335ba224f37403e8652b6693cf8b9da384b49
surendra12-lab <appalasurendra467@gmail.com>
2025-03-14 15:23:45 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,50 +1,31 @@
-﻿using System;
-
-static class hello
+﻿class hello
 { 
-    public static void showenvinormentDetails()
+    public static void consolefunctions()
     {
-        // priting drives in a machine
-        foreach(string s in Environment.GetLogicalDrives())
-        {
-            Console.WriteLine("drives is {0}", s);
-        }
-        Console.WriteLine(Environment.OSVersion);
-        Console.WriteLine(Environment.ProcessorCount);
-        Console.WriteLine(Environment.Version);
-        Console.WriteLine(Environment.ExitCode);
-        Console.WriteLine(Environment.Is64BitOperatingSystem);
-        Console.WriteLine(Environment.MachineName);
-        Console.WriteLine(Environment.NewLine);
-        //Console.WriteLine(Environment.ProcessId) in version 10.0 it is applicable
-        Console.WriteLine(Environment.SystemDirectory);
-        Console.WriteLine(Environment.UserName);
-        Console.WriteLine(Environment.CurrentManagedThreadId);
-        Console.WriteLine(Environment.HasShutdownStarted);
-        Console.WriteLine(Environment.WorkingSet);
-        Console.WriteLine(Environment.UserDomainName);
-        Console.WriteLine(Environment.TickCount);
-        Console.WriteLine(Environment.UserInteractive);
-        String desk_path=Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        Console.WriteLine(desk_path);
-
-        Console.WriteLine(Environment.GetEnvironmentVariable("path"));
-        Console.WriteLine(Environment.GetCommandLineArgs());
-        Environment.FailFast("critical error");
-
-        //Console.WriteLine(Environment.ExpandEnvironmentVariables);used to extend path like %temp%
-
+        System.Console.Beep();
+        System.Console.BackgroundColor = System.ConsoleColor.Green;
+        System.Console.WriteLine("hai");
+        System.Console.ForegroundColor = System.ConsoleColor.Blue;
+        System.Console.BufferHeight = 10000;
+        System.Console.BufferWidth = 1000;
+        System.Console.WriteLine(System.Console.Title);
+        System.Console.Title = "raju";
+        System.Console.WriteLine(System.Console.Title);
+        System.Console.WindowHeight = 600;
+        System.Console.WindowLeft = 0;
+        System.Console.WindowTop = 0;
+        System.Console.WindowWidth = 500;
+        System.Console.Clear();
+        string b=System.Console.ReadLine();
+       int a= System.Console.Read();
+        System.Console.WriteLine("{0},{1}", b, a);
+        System.Console.ReadKey();
     }
 }
-
-
 class main
 {
-     public static void Main(String[] args)
+    public static void Main(string[] args)
     {
-        hello.showenvinormentDetails();
-       // Console.WriteLine(Environment.StackTrace);
+        hello.consolefunctions();
     }
-
-
 }
